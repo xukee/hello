9b507a48879876c056a8fb6348ffcb3487e44ae5
xukee <243005972@qq.com>
2013-09-07 21:46:36 +0800

    t

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+// hello.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "stdafx.h"
+
+#include<iostream>
+using namespace std;
+void main()
+{
+  cout<<"helloworld! I am xukee(2012060050013)"<<endl;
+  system("pause");
+}
+
+
+
+
